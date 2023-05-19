d45df8231b293ff10b2ff9e99853e237b12e67ce
KaushalDhakal0 <kaushaldhakal90@gmail.com>
2023-05-19 14:55:05 +0545

    Auth handling login

--- controller/authController.js
+++ controller/authController.js
@@ -1,10 +1,29 @@
+const bcrypt = require("bcrypt");
+const User = require("../models/user");
+
+const login = async(req,res,next) => {
+    const {name,password, username, email} = req.body;
+    try{
+        const hashedPasswd = await bcrypt.hash(password,10);
+        // console.log("Hashed", hashedPasswd);
+        const newUser = new User({
+            username,
+            email,
+            name,
+            password: hashedPasswd
+        });
+        const user = await newUser.save();
+
+        res.json({
+            user
+        })
+
+    }catch(error){
+        console.log("Error Occured",error);
+    }
 
 
-const login = (req,res,next) => {
-    const {name,password} = req.body;
-    res.json({
-        name,password
-    })
+    
 }
 
 
